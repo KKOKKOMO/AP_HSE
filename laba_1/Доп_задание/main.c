0c6d3c14621a803a2e7ed3e534e2298a67d70b1e
Stepan <68470542+KKOKKOMO@users.noreply.github.com>
2024-10-19 19:56:18 +0300

    Обновлено доп задание

--- laba_1/Доп_задание/main.c
+++ laba_1/Доп_задание/main.c
@@ -77,19 +77,65 @@ int cbrt_bitwise(unsigned x) { //The book "Hacker's Delight" has algorithms for 
    return y;
 }
 
+int division(int dividend, int divisor, int origdiv, int * remainder)
+{
+    int quotient = 1;
+
+    if (dividend == divisor)
+    {
+        *remainder = 0;
+        return 1;
+    }
+
+    else if (dividend < divisor)
+    {
+        *remainder = dividend;
+        return 0;
+    }
+
+    while (divisor <= dividend)
+    {
+        divisor = divisor << 1;
+        quotient = quotient << 1;
+    }
+
+    if (dividend < divisor)
+    {
+        divisor >>= 1;
+        quotient >>= 1;
+    }
+
+    quotient = quotient + division(dividend - divisor, origdiv, origdiv, remainder);
+
+    return quotient;
+}
+
+
+
 
 int main(){
-    printf("SQRT_BITWISE: %d\n", sqrt_bitwise(4));
+    int rem = 0;
+    int n = 121;
+    int d = 2;
+    printf("SQRT_BITWISE: %d\n", sqrt_bitwise(25));
     printf("LOG2_BITWISE: %d\n", log2_bitwise(8));
     printf("CBRT_BITWISE: %d\n", cbrt_bitwise(27));
     printf("SQUARE_BITWISE: %d\n", square_bitwise(4));
+    printf("Quotient : %d\n", division(n, d, d, &rem));
+    printf("Remainder: %d\n", rem);
 
 
     int a, b, c;
+    int result;
+    
     printf("Введите значения a, b и c: ");
     scanf("%d %d %d", &a, &b, &c);
 
     int sqrt_a = sqrt_bitwise(a);
-    int log_b_suare_c = log2_bitwise(b) - square_bitwise(c);
+    int cbrt_log_b_suare_c = cbrt_bitwise(log2_bitwise(b) - square_bitwise(c));
+    result = division(sqrt_a, cbrt_log_b_suare_c, cbrt_log_b_suare_c, &rem);
+    printf("RESULT_BY_BITWISE: %d\n", result);
+    result = sqrt_a/cbrt_log_b_suare_c;
+    printf("RESULT: %d\n", result);
     return 0;
 }
