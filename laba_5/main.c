19e2a5e33efcf8ffce65b3156eea78ac649ddddd
Stepan <s.bakhmutov@bk.ru>
2024-12-14 04:25:25 +0300

    Update main.c

--- laba_5/main.c
+++ laba_5/main.c
@@ -6,28 +6,28 @@
 #define MAX_LEN_WORD 11
 
 
-size_t first_dot_idx(const char *original_str){
-    size_t dot_idx = 0;
-    size_t str_len = strlen(original_str);
+int first_dot_idx(const char *original_str){
+    int dot_idx = 0;
+    int str_len = strlen(original_str);
 
-    for (size_t i = 0; i < str_len; i++)
+    for (int i = 0; i < str_len; i++)
     {
         if (original_str[i] == '.'){
             //printf("dot_idx: %lu\n", dot_idx);
-            return ++dot_idx;
+            return i;
         }
-        dot_idx++;
     }
-    //printf("dot_idx: %lu\n", dot_idx);
+    printf("str_len: %d\n", str_len);
+
     return str_len;
 }
 
-size_t count_words_in_sentence(const char *original_str){
+int count_words_in_sentence(const char *original_str){
     if (original_str == NULL)
     return 0;
 
-    size_t number_of_words = 0;
-    size_t dot_idx = first_dot_idx(original_str);
+    int number_of_words = 0;
+    int dot_idx = first_dot_idx(original_str);
     //printf("dot_idx: %lu\n", dot_idx);
     int len_original_str = strlen(original_str);
     char previos_symbol = original_str[0];
@@ -35,7 +35,7 @@ size_t count_words_in_sentence(const char *original_str){
     if (previos_symbol == '\n' || previos_symbol == '\0')
     return 0;
     
-    for (size_t i = 0; i < dot_idx; i++)
+    for (int i = 0; i < dot_idx; i++)
     {   
         if ((original_str[i] == ' ' || original_str[i] == '\n' || original_str[i] == '\0' || original_str[i] == '.'|| original_str[i] == '\t')
             && previos_symbol != ' ' && previos_symbol != '.' && previos_symbol != '\t')
@@ -47,13 +47,13 @@ size_t count_words_in_sentence(const char *original_str){
 
     if (number_of_words > 30)
         {
-            printf("TOO MUCH WORDS GIVE ME 6))\n");
+            printf("TOO MUCH WORDS GIVE ME 6 for labs))\n");
             return 0;
         }
     return number_of_words;
 }
 
-char **exctract_words_from_sentence(const char *original_str, size_t number_of_words, size_t dot_idx){
+char **exctract_words_from_sentence(const char *original_str, int number_of_words, int dot_idx){
     if (number_of_words == 0)
     {
         printf("The sentence has no words\n");
@@ -62,10 +62,11 @@ char **exctract_words_from_sentence(const char *original_str, size_t number_of_w
     
     char **words_array = NULL;
     char *word = NULL;
-    //size_t dot_idx = first_dot_idx(original_str);
+    //int dot_idx = first_dot_idx(original_str);
     //printf("dot_idx: %lu\n", dot_idx);
     
-    size_t size_origin_str = strlen(original_str);
+    int size_origin_str = strlen(original_str);
+    //printf("size_origin_str: %d\n", size_origin_str);
     char copy_original_str[dot_idx+2]; // 6 8
     strncpy(copy_original_str, original_str, dot_idx+2);
     //printf("LAST STR EL %c\n", copy_original_str[dot_idx+2]);
@@ -75,9 +76,8 @@ char **exctract_words_from_sentence(const char *original_str, size_t number_of_w
     //printf("COPY STR <%s>\n", copy_original_str);
     word = strtok(copy_original_str, " \t.\n");
     words_array = (char**)malloc(number_of_words * sizeof(char*));
-    for (size_t i = 0; i < number_of_words; i++)
+    for (int i = 0; i < number_of_words; i++)
     {
-        
         words_array[i] = (char*)malloc(MAX_LEN_WORD * sizeof(char));
         strncpy(words_array[i], word, strlen(word) + 1);
         //printf("i:%zu ADDRESS: %p\n", i ,words_array[i]);
@@ -87,16 +87,60 @@ char **exctract_words_from_sentence(const char *original_str, size_t number_of_w
     return words_array;
 }
 
-void free_words_array(char **words_array, int number_of_words) {
-    for (int i = 0; i < number_of_words; i++) {
-        free(words_array[i]);
+void find_correct_word(char ***words_array, int *number_of_words){
+    char **word_arr = *words_array;
+    int j = 0;
+    for (int i = 0; i < *number_of_words; i++)
+    {
+        int curr_len_word = strlen(word_arr[i]);
+        if (curr_len_word < 10)
+        {
+            if (j != i)
+            {
+                strncpy(word_arr[j], word_arr[i], curr_len_word);
+                word_arr[j][curr_len_word] = '\0';
+            }
+            j++;
+        }
     }
-    free(words_array);
+    *number_of_words = j;
+    //printf("*number_of_words %d\n", *number_of_words);
+    *words_array = (char**)realloc(word_arr, *number_of_words*sizeof(char*));
 }
+
+void delete_duplicate_words(char ***words_array, int *number_of_words){
+    char **word_arr = *words_array;
+    char *last_word = word_arr[*number_of_words-1];
+    int j = 0;
+
+    for (int i = 0; i < *number_of_words; i++)
+    {
+        int curr_len_word = strlen(word_arr[i]);
+        if (strcmp(word_arr[i], last_word) != 0)
+        {
+            if (j != i)
+            {
+                strncpy(word_arr[j], word_arr[i], curr_len_word);
+                word_arr[j][curr_len_word] = '\0';
+            }
+            j++;
+        }
+    }
+    *number_of_words = j;
+    *words_array = (char**)realloc(word_arr, *number_of_words*sizeof(char*));
+}
+
+void free_words_array(char **words_array, int number_of_words) {
+        for (int i = 0; i < number_of_words; i++)
+        free(words_array[i]);
+        free(words_array);
+    }
 
 void print_final_sentence(char **words_array, int number_of_words){
     
 }
+
+
 
 
 int main(){
@@ -105,27 +149,28 @@ int main(){
         char **words = NULL;
         char origin_str[MAX_STR_LEN];
         fgets(origin_str, MAX_STR_LEN-1, stdin);
-        size_t dot_idx = first_dot_idx(origin_str);
+        int dot_idx = first_dot_idx(origin_str);
 
-        size_t size_origin_str = strlen(origin_str);
-        char copy_origin_str[size_origin_str+1]; //VLA ARRAY? 
-        strncpy(copy_origin_str, origin_str, size_origin_str+1);
+        int size_origin_str = strlen(origin_str);
+        //char copy_origin_str[size_origin_str+1]; //VLA ARRAY? 
+        //strncpy(copy_origin_str, origin_str, size_origin_str+1);
 
-        size_t number_of_words = count_words_in_sentence(origin_str);
+        int number_of_words = count_words_in_sentence(origin_str);
         printf("number_of_words %zu\n", number_of_words);
 
         words = exctract_words_from_sentence(origin_str, number_of_words, dot_idx);
-        if (words == NULL)
-        {
-            free_words_array(words, number_of_words);
-            origin_str[0] = '\0';
-            /* code */
-        }
+        //printf("find_correct_word %d\n", find_correct_word(words, &number_of_words));
+        find_correct_word(&words, &number_of_words);
+        printf("CURR number_of_words %zu\n", number_of_words);
+        delete_duplicate_words(&words, &number_of_words);
+        printf("CURR number_of_words %zu\n", number_of_words);
         
-        for (size_t i = 0; i < number_of_words; i++)
+        for (int i = 0; i < number_of_words; i++)
         {
             printf("Idx: <%lu> || WORD: <%s> - LEN WORD[%lu]: %lu\n", i+1, words[i], i, strlen(words[i]));
         }
+
+        
 
         //printf("%s\n", origin_str);
         //printf("%s\n", copy_origin_str);
@@ -133,6 +178,7 @@ int main(){
         //printf("%lu\n", strlen(origin_str));
         //printf("%lu\n", strlen(copy_origin_str));
 
+        if (words != NULL)
         free_words_array(words, number_of_words);
         origin_str[0] = '\0';
     }
@@ -141,7 +187,7 @@ int main(){
     //char origin_str[MAX_STR_LEN];
     //fgets(origin_str, MAX_STR_LEN-1, stdin);
 //
-    //size_t size_origin_str = strlen(origin_str);
+    //int size_origin_str = strlen(origin_str);
     //char copy_origin_str[size_origin_str+1]; //VLA ARRAY? 
     //strncpy(copy_origin_str, origin_str, size_origin_str+1);
 //
